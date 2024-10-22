86e359782f71987b7c403d8f3849c8feee07a712
mathilde pinon <mth.pinon@gmail.com>
2024-10-22 15:22:44 +0200

    plotting results

--- densitysplit/__init__.py
+++ densitysplit/__init__.py
@@ -1,6 +1,6 @@
 from ._version import __version__
 from .utils import BaseClass
 from .catalog_data import Data
-from .density_split import DensitySplit
+from .density_split import DensitySplit, CountInCellsDensitySplitMeasurement
 from .base_model import BaseTwoPointCorrelationFunctionModel, SmoothedTwoPointCorrelationFunctionModel
 from .gaussian_model import SmoothedGaussianDensityModel, GaussianDensitySplitsModel
